2a5a7ef418e502113a7b5c15db665795348653f7
Sam Peterson <samllama55@gmail.com>
2022-09-16 23:59:32 -0700

    CHANGE: Poc with axios.

--- src/server/router/example.ts
+++ src/server/router/example.ts
@@ -1,15 +1,37 @@
-import { createRouter } from "./context";
-import { z } from "zod";
+import { createRouter } from './context';
+import { z } from 'zod';
+import axios from 'axios';
 
-export const exampleRouter = createRouter().query("hello", {
-  input: z
-    .object({
-      text: z.string().nullish(),
-    })
-    .nullish(),
-  resolve({ input }) {
-    return {
-      greeting: `Hello ${input?.text ?? "world"}`,
-    };
-  },
+const DATABASE_URL = 'https://jsonplaceholder.typicode.com';
+
+const ZodToDo = z.object({
+  userId: z.number(),
+  id: z.number(),
+  title: z.string(),
+  completed: z.boolean(),
 });
+
+type ToDo = z.infer<typeof ZodToDo>;
+
+export const exampleRouter = createRouter()
+  .query('hello', {
+    input: z
+      .object({
+        text: z.string().nullish(),
+      })
+      .nullish(),
+    resolve({ input }) {
+      return {
+        greeting: `Hello ${input?.text ?? 'world'}`,
+      };
+    },
+  })
+  .query('getTodos', {
+    async resolve(): Promise<ToDo[]> {
+      const response = await axios
+        .get<ToDo[]>(`${DATABASE_URL}/todos`)
+        .then((data) => data.data.map((dataItem) => ZodToDo.parse(dataItem)));
+
+      return response;
+    },
+  });
